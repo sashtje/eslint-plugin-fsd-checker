258a5884b8d20f3e1d57ccefc4c023722ede633b
sashtje <sashtje.lefevre@gmail.com>
2023-09-26 16:32:20 +0300

    feat: add alias support

--- tests/lib/rules/path-checker.js
+++ tests/lib/rules/path-checker.js
@@ -16,16 +16,29 @@ const rule = require("../../../lib/rules/path-checker"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+});
 ruleTester.run("path-checker", rule, {
   valid: [
-    // give me some code that won't trigger a warning
+    {
+      filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '../../model/slices/addCommentFormSlice'",
+      errors: [],
+    },
   ],
 
   invalid: [
     {
-      code: "",
-      errors: [{ message: "Fill me in.", type: "Me too" }],
+      filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "Within one slice, paths must be relative" }],
+    },
+    {
+      filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article',
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
+      errors: [{ message: "Within one slice, paths must be relative" }],
+      options: [{alias: '@'}]
     },
   ],
 });

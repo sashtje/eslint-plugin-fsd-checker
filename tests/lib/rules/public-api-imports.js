b430edb6aa1c6efe411e0ed782b9e82555c5ed8a
sashtje <sashtje.lefevre@gmail.com>
2023-09-27 22:51:49 +0300

    feat: add fix for public-api-imports rule

--- tests/lib/rules/public-api-imports.js
+++ tests/lib/rules/public-api-imports.js
@@ -7,6 +7,7 @@
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
+const {PUBLIC_ERROR, TESTING_PUBLIC_ERROR} = require("../../../lib/const/const");
 
 const rule = require("../../../lib/rules/public-api-imports"),
   RuleTester = require("eslint").RuleTester;
@@ -45,40 +46,45 @@ ruleTester.run("public-api-imports", rule, {
 
   invalid: [
     {
-      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice'",
-      errors: [{ message: "Absolute import is only allowed from Public API (index.ts or testing.ts)" }],
+      code: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article/model/slices/addCommentFormSlice';",
+      errors: [{ messageId: PUBLIC_ERROR }],
+      output: "import { addCommentFormActions, addCommentFormReducer } from 'entities/Article';"
     },
     {
-      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice'",
-      errors: [{ message: "Absolute import is only allowed from Public API (index.ts or testing.ts)" }],
-      options: aliasOptions
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/model/slices/addCommentFormSlice';",
+      errors: [{ messageId: PUBLIC_ERROR }],
+      options: aliasOptions,
+      output: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article';"
     },
     {
       filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article\\file.test.ts',
-      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-      errors: [{ message: "This data must be imported from Public API (index.ts)" }],
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing';",
+      errors: [{ messageId: TESTING_PUBLIC_ERROR }],
       options: [{
         alias: '@',
         testFiles: []
-      }]
+      }],
+      output: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article';"
     },
     {
       filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article\\file.test.ts',
-      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx'",
-      errors: [{ message: "Absolute import is only allowed from Public API (index.ts or testing.ts)" }],
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing/file.tsx';",
+      errors: [{ messageId: PUBLIC_ERROR }],
       options: [{
         alias: '@',
         testFiles: ['**/*.test.ts', '**/*.test.tsx']
-      }]
+      }],
+      output: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing';"
     },
     {
       filename: 'C:\\Desktop\\javascript\\production-project\\src\\entities\\Article\\file.ts',
-      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing'",
-      errors: [{ message: "This data must be imported from Public API (index.ts)" }],
+      code: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article/testing';",
+      errors: [{ messageId: TESTING_PUBLIC_ERROR }],
       options: [{
         alias: '@',
         testFiles: ['**/*.test.ts', '**/*.test.tsx']
-      }]
+      }],
+      output: "import { addCommentFormActions, addCommentFormReducer } from '@/entities/Article';"
     },
   ],
 });
